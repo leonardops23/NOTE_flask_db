76bbd7319a77d3dfacbb46b67a7995c3722d8034
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-09-18 18:09:52 -0500

    add congif for app

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,12 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config
+    app.config['SECRET_KEY'] = 'LEO@alskw$___23333d'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
