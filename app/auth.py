59087cb3f5874badae8447cbc4b72a1ee2e37a76
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-09-19 20:43:51 -0500

    add new endpoints

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('base.html')
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return '<h1>Logout</h1>'
+    return render_template('login.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return '<h1>Sign up</h1>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firsName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characteres.', category='error')
+        elif len(firsName) < 2:
+            flash('First Name  must greater than 1 characters.',
+            category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Email must be at least 7 characteres', category='error')
+        else:
+            flash('Account created!', category='success')
+    return render_template('sing-up.html')
